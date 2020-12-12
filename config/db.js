fe72c678f4258c8a20bdec9b84c400442e75cbbc
Deepak Sindhwani <dsindhwani55@gmail.com>
2020-12-12 18:43:22 +0530

    Fixed Db Configuration Auth

--- config/db.js
+++ config/db.js
@@ -3,14 +3,11 @@ const keys = require("./keys.js");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      keys.NODE_ENV === "production" ? keys.MONGO_URI_PROD : keys.MONGO_URI_DEV,
-      {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useCreateIndex: true,
-      }
-    );
+    const conn = await mongoose.connect(keys.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
     console.log("Mongodb Connected :", conn.connection.host);
   } catch (e) {
     console.log("Error is", e.message);
